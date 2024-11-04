89c2405e2d93ecd439b89082d637c520d4cd9ec4
Mahib Khan <120155048+mahib2121@users.noreply.github.com>
2024-11-05 00:22:27 +0600

    Add files via upload

---
+++ W2/D2/A_Clear_Day.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main (){
+    
+
+    int x , y;
+    cin>> x >>y ;
+    int p= x+y;
+    int clear = 7-p;
+    cout << clear ;
+    return 0;
+}
