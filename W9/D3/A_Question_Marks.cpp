3cde8e9cbd0bb72e4e1044bc094cb84e2240765d
Mahib Khan <120155048+mahib2121@users.noreply.github.com>
2024-12-25 00:23:17 +0600

    Update A_Question_Marks.cpp

--- W9/D3/A_Question_Marks.cpp
+++ W9/D3/A_Question_Marks.cpp
@@ -8,7 +8,21 @@ int main (){
     cin >> t;
     while (t--)
     {
-      
+       int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        map<char, int> mp;
+        for (char c : s)
+        {
+            mp[c]++;
+        }
+        int result = 0;
+        for (char c : "ABCD")
+        {
+            result += min(n, mp[c]);
+        }
+        cout << result << endl;
     }
     return 0;
 }
