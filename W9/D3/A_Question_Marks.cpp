45097ac871862b02ea8366beca441b65b0902088
Mahib Khan <120155048+mahib2121@users.noreply.github.com>
2024-12-24 23:42:54 +0600

    Update A_Question_Marks.cpp

--- W9/D3/A_Question_Marks.cpp
+++ W9/D3/A_Question_Marks.cpp
@@ -8,16 +8,7 @@ int main (){
     cin >> t;
     while (t--)
     {
-      int n;
-        cin >> n;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-        sort(a.begin(), a.end(), greater<int>());
-        int x = (n - 1) / 2;
-        cout << a[x] << endl;
+      
     }
     return 0;
 }
