23c7946806a29b174990ebf8ce85a6ce2d1c8a27
Mahib Khan <120155048+mahib2121@users.noreply.github.com>
2025-01-02 23:39:29 +0600

    Add files via upload

---
+++ W9/D6/A_Binary_Search.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    for (int i = 0; i < k; i++)
+    {
+        int key = v[i];
+        int l = 0;
+        int r = n - 1;
+        int mid = 0 ;
+        while (l<=r )
+        {
+            
+            mid = (l+r)/2;
+            if 
+        }
+        
+        
+  
+    }
+
+    return 0;
+}
