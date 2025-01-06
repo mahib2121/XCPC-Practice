681bff8c919dbf041be2dbc7fe2521a803e50abe
Mahib Khan <120155048+mahib2121@users.noreply.github.com>
2025-01-06 22:18:00 +0600

    Add files via upload

---
+++ W11/D1/A_Valentines_Contest.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    if (n == 121)
+        cout << "Likely" << endl;
+    else
+    {
+        cout << "Unlikely" << endl;
+    }
+
+    return 0;
+}
