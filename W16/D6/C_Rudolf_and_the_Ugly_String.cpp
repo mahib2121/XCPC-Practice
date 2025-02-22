97c7ae96a5879c178010988a8804727793cbae64
Mahib Khan <120155048+mahib2121@users.noreply.github.com>
2025-02-22 22:17:51 +0600

    Add files via upload

--- W16/D6/C_Rudolf_and_the_Ugly_String.cpp
+++ W16/D6/C_Rudolf_and_the_Ugly_String.cpp
@@ -3,6 +3,36 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int ans = 0;
+        int i = 0;
 
+        while (i < n)
+        {
+            if (i <= n - 5 && s.substr(i, 5) == "mapie")
+            {
+                ans++;
+                i += 5;
+            }
+            else if (i <= n - 3 && (s.substr(i, 3) == "map" || s.substr(i, 3) == "pie"))
+            {
+                ans++;
+                i += 3;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        cout << ans << endl;
+    }
     return 0;
 }
