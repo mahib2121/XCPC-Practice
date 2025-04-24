a4fb5df0267cc0b6df29708553959884eec5b810
Mahib Khan <120155048+mahib2121@users.noreply.github.com>
2025-04-24 21:06:56 +0600

    Add files via upload

---
+++ Codeforces Round 1020 (Div. 3)/A_Dr_TC.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >>
+            n;
+        string s;
+        cin >> s;
+
+        int count1 = 0;
+
+        for (char c : s)
+        {
+            if (c == '1')
+            {
+                count1++;
+            }
+        }
+
+        int res = (n - count1) + (count1 * (n - 1));
+        cout << res << endl;
+    }
+    return 0;
+}
